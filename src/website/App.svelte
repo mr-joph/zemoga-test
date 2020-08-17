db52fe5045c36b384feafbf809dd3ef12c8d0af7
Joph <>
2020-08-17 14:18:17 -0500

    new: adding basic footer layout

--- src/website/App.svelte
+++ src/website/App.svelte
@@ -1,6 +1,6 @@
 <script>
 import Header from "components/header/Header";
-
+import Footer from "components/footer/Footer";
 let word = "begin";
 
 </script>
@@ -8,9 +8,20 @@ let word = "begin";
 <style type="text/scss">
 @import "styles/theme.scss";
 
+main {
+  margin: 0 auto;
+  max-width: 1025px;
+  position: relative;
+}
+
 </style>
 
 <div>
   <Header />
-  Testing our setup!! YAY!
+
+  <main>
+    Testing our setup!! YAY!
+    <Footer />
+  </main>
+
 </div>
--- src/website/components/footer/Footer.svelte
+++ src/website/components/footer/Footer.svelte
@@ -2,6 +2,41 @@
 </script>
 
 <style type="text/scss">
+@import "styles/theme.scss";
+
+footer {
+  color: $darkest-color;
+  display: flex;
+  font-family: $font-light;
+  font-size: 12px;
+  width: 100%;
+  
+  & > ul {
+    display: flex;
+  }
+
+  & > div {
+    display: flex;
+  }
+}
+
 </style>
 
-<footer>footer</footer>
+<footer>
+  <ul>
+    <li>Terms and Conditions</li>
+    <li>Privacy Policy</li>
+    <li>Contact Us</li>
+  </ul>
+
+  <div>
+    <span>
+      Follow Up
+    </span>
+    <ul>
+      <li>Facebook icon</li>
+      <li>Twitter icon</li>
+    </ul>
+  </div>
+
+</footer>
